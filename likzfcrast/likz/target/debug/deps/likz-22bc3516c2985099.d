dd273779499d1159d35e5e8d89b656593cbfba29
ECROF88 <maomao8672@outlook.com>
2024-12-30 16:01:33 +0800

    new file:   .gitignore 	new file:   likzfcrast/likz/Cargo.lock 	new file:   likzfcrast/likz/Cargo.toml 	new file:   likzfcrast/likz/src/main.rs

---
+++ likzfcrast/likz/target/debug/deps/likz-22bc3516c2985099.d
@@ -0,0 +1,5 @@
+C:\Users\D1mo\Documents\ZaymyRsila\likzfcrast\likz\target\debug\deps\likz-22bc3516c2985099.exe: src/main.rs
+
+C:\Users\D1mo\Documents\ZaymyRsila\likzfcrast\likz\target\debug\deps\likz-22bc3516c2985099.d: src/main.rs
+
+src/main.rs:
---
+++ likzfcrast/likz/target/debug/likz.d
@@ -0,0 +1,1 @@
+C:\Users\D1mo\Documents\ZaymyRsila\likzfcrast\likz\target\debug\likz.exe: C:\Users\D1mo\Documents\ZaymyRsila\likzfcrast\likz\src\main.rs
