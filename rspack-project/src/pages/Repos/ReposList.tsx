9fc84e59eb1cb6fe5793d585faad8c8c87493ae5
ECROF88 <maomao8672@outlook.com>
2025-02-06 23:13:49 +0800

    add new

--- rspack-project/src/pages/Repos/ReposList.tsx
+++ rspack-project/src/pages/Repos/ReposList.tsx
@@ -1,9 +1,13 @@
 // src/components/Repos/RepoList.tsx
 import React, { useState } from 'react';
-import { List, Typography, Divider, Button } from 'antd';
+import { List, Typography, Divider, Button, Dropdown } from 'antd';
 import useRepoData from './hooks/useRepoData';
-import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Modal } from 'antd';
+import {
+  PlusOutlined,
+  DeleteOutlined,
+  EllipsisOutlined,
+  EditOutlined,
+} from '@ant-design/icons';
 import AddRepos from './AddRepos';
 const { Title } = Typography;
 
@@ -27,7 +31,7 @@ const ReposList: React.FC = () => {
         <Button
           danger
           icon={<DeleteOutlined />}
-          disabled={!selectedRepo}
+          // disabled={!selectedRepo}
           onClick={() => {
             /* 实现删除逻辑 */
           }}
@@ -37,6 +41,77 @@ const ReposList: React.FC = () => {
       </div>
       <Divider className="my-0" />
       <List
+        dataSource={repos}
+        // className="flex-1"
+        className="flex-auto"
+        renderItem={(repo) => (
+          <List.Item
+            key={repo.id}
+            onClick={(e) => {
+              // 防止点击下拉菜单时触发选择仓库
+              if (!(e.target as HTMLElement).closest('.ant-dropdown-trigger')) {
+                setSelectedRepo(repo);
+              }
+            }}
+            className={`
+        border-amber-100
+        border-2
+        px-6 py-3 
+        cursor-pointer 
+        transition-all duration-200 ease-in-out
+        hover:bg-gray-100
+        ${
+          selectedRepo?.id === repo.id
+            ? 'bg-green-100 border-l-4 border-green-500'
+            : 'hover:border-l-6 hover:border-gray-300'
+        }
+      `}
+            actions={[
+              <Dropdown
+                key="more"
+                trigger={['click']}
+                menu={{
+                  items: [
+                    {
+                      key: 'edit',
+                      icon: <EditOutlined />,
+                      label: '编辑',
+                      onClick: () => {
+                        console.log('编辑仓库:', repo);
+                      },
+                    },
+                    {
+                      key: 'delete',
+                      icon: <DeleteOutlined />,
+                      label: '删除',
+                      danger: true,
+                      onClick: () => {
+                        console.log('删除仓库:', repo);
+                      },
+                    },
+                  ],
+                }}
+              >
+                <EllipsisOutlined className="text-gray-500 hover:text-gray-700" />
+              </Dropdown>,
+            ]}
+          >
+            <List.Item.Meta
+              title={
+                <span
+                  className={`
+            font-medium
+            ${selectedRepo?.id === repo.id ? 'text-green-700' : 'text-gray-700'}
+          `}
+                >
+                  {repo.name}
+                </span>
+              }
+            />
+          </List.Item>
+        )}
+      />
+      {/* <List
         itemLayout="horizontal"
         dataSource={repos}
         className="flex-1"
@@ -72,7 +147,7 @@ const ReposList: React.FC = () => {
             />
           </List.Item>
         )}
-      />
+      /> */}
       <AddRepos isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
   );
