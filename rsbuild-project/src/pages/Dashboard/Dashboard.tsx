c38afdb235c5758bb29a48f422770b26a6770075
ECROF88 <maomao8672@outlook.com>
2025-03-15 16:09:00 +0800

    test format

--- rsbuild-project/src/pages/Dashboard/Dashboard.tsx
+++ rsbuild-project/src/pages/Dashboard/Dashboard.tsx
@@ -1,27 +1,25 @@
-import { Layout } from 'antd'
-import React from 'react'
-import { Outlet } from 'react-router'
-import Footer from './Footer'
-import Header from './Header'
-import Sidebar from './Sidebar'
+import { Layout } from "antd";
+import type React from "react";
+import { Outlet } from "react-router";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-const { Content } = Layout
+const { Content } = Layout;
 
 const DashboardLayout: React.FC = () => {
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sidebar />
-      <Layout>
-        <Header />
-        <Content className="m-4 p-4 bg-auto">
-          <Outlet />
-          {' '}
-          {/* 渲染匹配的子路由组件 */}
-        </Content>
-        <Footer />
-      </Layout>
-    </Layout>
-  )
-}
+	return (
+		<Layout style={{ minHeight: "100vh" }}>
+			<Sidebar />
+			<Layout>
+				<Header />
+				<Content className="m-4 p-4 bg-auto">
+					<Outlet /> {/* 渲染匹配的子路由组件 */}
+				</Content>
+				<Footer />
+			</Layout>
+		</Layout>
+	);
+};
 
-export default DashboardLayout
+export default DashboardLayout;
